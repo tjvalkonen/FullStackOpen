2002f17db8b37addae6069533505652f4da69f26
tjvalkonen <tomas.valkonen@gmail.com>
2025-03-14 07:27:29 +0200

    Working on 5.1.-5.4

--- Osa5/bloglist-frontend/src/App.jsx
+++ Osa5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +20,139 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    // console.log('logging in with', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong username or password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const onClickLogout = () => {
+    window.localStorage.clear()
+    window.location.reload(false)
+  }
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      title:<input
+        value={newTitle}
+        onChange={handleTitleChange}
+      />
+      <br></br>
+      author:<input
+        value={newAuthor}
+        onChange={handleAuthorChange}
+      />
+      <br></br>
+      url:<input
+        value={newUrl}
+        onChange={handleUrlChange}
+      />
+      <br></br>
+      <button type="submit">create</button>
+    </form>  
+  )
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+  
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewTitle('')
+        setNewAuthor('')
+        setNewUrl('')
+      })
+  }
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value)
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <Notification message={errorMessage} />
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+    )
+  }
+
   return (
     <div>
+      <Notification message={errorMessage} />
       <h2>blogs</h2>
+      <div>
+        <p>{user.name} logged in</p>
+        <button onClick={onClickLogout}>logout</button>
+
+        <h2>create new</h2>
+        {blogForm()}
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
