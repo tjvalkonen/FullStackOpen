f77aa05f529c1d19970a20394ef813ce376c4460
tjvalkonen <tomas.valkonen@gmail.com>
2025-03-17 02:57:22 +0200

    Blog visibility

--- Osa5/bloglist-frontend/src/components/Blog.jsx
+++ Osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,20 +1,50 @@
-import Togglable from './Togglable'
+import { useState } from 'react'
 
-const Blog = ({ blog, username }) => (
+const Blog = ({ blog, username }) => {
+  const [visible, setVisible] = useState(false)
 
-  
-  <div className="blog">
-    {blog.title} {blog.author}
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div>
+      <div className="blog">
+      <div style={hideWhenVisible}>
+      {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+      {blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button>
+      <br></br>
+        {blog.url}
+        <br></br>
+        likes {blog.likes}
+        <br></br>
+      {username}
+    </div>
+  </div>
+  </div>
+  )
+  /*
+  return (
+    <div className="blog">
+    {blog.title} {blog.author} <button onClick={toggleVisibility}>{label}</button>
 
     <Togglable buttonLabel='view' buttonLabelCancel='hide' >
     {blog.url}
     <br></br>
      likes {blog.likes}
      <br></br>
+
       {username}
     </Togglable>
-  </div>  
-)
+  </div> 
+  )
+ */
+}
 
 
 
