bb809353378b1f182313073c2b44dbca65f7ec0c
tjvalkonen <tomas.valkonen@gmail.com>
2026-01-26 20:55:07 +0200

    9.3

--- Osa9/typescript/bmiCalculator.ts
+++ Osa9/typescript/bmiCalculator.ts
@@ -1,7 +1,25 @@
 const calculateBmi = (height: number, mass: number) => {
-  // console.log(a * b);
   const bmi = mass/((height/100)*(height/100))
+  var resultText
+  if(bmi < 16 ){
+    resultText = "Underweight (Severe thinness)"
+  } else if (16 <= bmi && bmi < 17) {
+    resultText = "Underweight (Moderate thinness)"
+  } else if (17 <= bmi && bmi < 18.5) {
+    resultText = "Underweight (Mild thinness)"
+  } else if (18.5 <= bmi && bmi < 25) {
+    resultText = "Normal range"
+  } else if (25 <= bmi && bmi < 30) {
+    resultText = "Overweight (Pre-obese)"
+  } else if (30 <= bmi && bmi < 35) {
+    resultText = "Obese (Class I)"
+  } else if (35 <= bmi && bmi < 40) {
+    resultText = "Obese (Class II)"
+  } else if (40 <= bmi) {
+    resultText = "Obese (Class III)"
+  } 
 
+  /*
   if(bmi < 16 ){
     return "Underweight (Severe thinness)"
   } else if (16 <= bmi && bmi < 17) {
@@ -18,8 +36,14 @@ const calculateBmi = (height: number, mass: number) => {
     return "Obese (Class II)"
   } else if (40 <= bmi) {
     return "Obese (Class III)"
-  } 
-
+  }
+  */
+  console.log(resultText);
 }
 
-console.log(calculateBmi(180, 74));
+const height: number = Number(process.argv[2])
+const mass: number = Number(process.argv[3])
+
+calculateBmi(height, mass)
+
+// console.log(calculateBmi(180, 74));
--- Osa9/typescript/exerciseCalculator.ts
+++ Osa9/typescript/exerciseCalculator.ts
@@ -8,10 +8,44 @@ interface Result {
     average: number;
 }
 
+interface InputValues {
+    targetValue: number;
+    dailyHours: Array<number>;
+}
+
+const parseArguments = (args: string[]): InputValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+
+
+    const daysSplice = args.splice(3)
+    // console.log(daysSplice)
+
+    const days = daysSplice.map(Number);
+    // Check if numbers! Not done?
+    // console.log(days.some((d) => typeof d !== "number" || isNaN(d)))
+
+
+    // typeof x === "number"
+    // console.log(days)
+
+    // if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    if (!isNaN(Number(args[2])) && !(days.some((d) => typeof d !== "number" || isNaN(d)))) {
+      return {
+        targetValue: Number(args[2]),
+        // only first of array must fix!
+        dailyHours: days
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+
+}
+
 const calculateExercises = (dailyHours: number[], targetValue: number): Result => {
     // const days = 2
     const periodLength = dailyHours.length
     // check zero days
+    // console.log(dailyHours)
     const trainingDays = dailyHours.filter(val => val !==0).length
     
     const totalTime = dailyHours.reduce((acc, curr) => acc + curr, 0)
@@ -22,7 +56,7 @@ const calculateExercises = (dailyHours: number[], targetValue: number): Result =
 
     if(average < (targetValue/2)) {
         rating = 1
-        ratingDescription = 'not good. You can do better :)'
+        ratingDescription = 'Not good. You can do better :)'
     } else if(average >= (targetValue/2) && average < targetValue) {
         rating = 2
         ratingDescription = 'not too bad but could be better'
@@ -31,16 +65,43 @@ const calculateExercises = (dailyHours: number[], targetValue: number): Result =
         ratingDescription = 'Target met! Most excellent!'
     }
 
+    
+    // --> console log
     return {
         periodLength: periodLength,
         trainingDays: trainingDays,
         success: (average >= targetValue),
         rating: rating,
         ratingDescription: ratingDescription,
-        // check zero days
         target: targetValue,
         average: average
     }
+    /*
+    return {
+        periodLength: periodLength,
+        trainingDays: trainingDays,
+        success: (average >= targetValue),
+        rating: rating,
+        ratingDescription: ratingDescription,
+        target: targetValue,
+        average: average
+    }
+    */
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+// const targetValue: number = Number(process.argv[2])
+// let dailyHours: Array<number>,(process.argv[3])
+
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+
+try {
+  const { targetValue, dailyHours } = parseArguments(process.argv);
+  console.log(calculateExercises(dailyHours, targetValue));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
